60329d3edc82cc0d1d8658ffb59cc64ab03af905
Baldur Blöndal <baldurpet@gmail.com>
2014-03-07 07:10:19 +0100

    property for homomorphism

--- SHE.hs
+++ SHE.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE ViewPatterns #-}
 
+-- http://people.csail.mit.edu/shaih/pubs/FHE-winter-school.pdf
+-- http://research.microsoft.com/pubs/146975/ihe.pdf
+
 import Control.Applicative
 import System.Random
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
 import Text.Printf
 import Data.Word
+import Control.Monad
 
 -- data Parameters = Param {
 --   γ ∷ Integer,    -- Bit-length of integers in public key
@@ -75,7 +79,6 @@ encrypt η (SharedSecretKey p) (toInt → m) = do
   
   -- random small R
   r ← randomRIO rRange
-  -- printf "r: %d\n" r
   
   let qExponent ∷ Integer
       qExponent = round (fromIntegral (η ^ 3))
@@ -85,36 +88,54 @@ encrypt η (SharedSecretKey p) (toInt → m) = do
 
   -- random large Q
   q ← randomRIO qRange
-  
-  -- printf "q: %d\n" q
 
-  
-  
-  return $ (p*q + 2*r + m ∷ Integer)
+  if 2*r < p`div`2
+    then return $ (p*q + 2*r + m ∷ Integer)
+    else error "foobar"
 
 decrypt ∷ SharedSecretKey → Integer → Bit
 decrypt (SharedSecretKey p) c = fromInt (c `mod` p `mod` 2)
 
 prop_encdec ∷ Word8 → Bit → Property
 prop_encdec seed bit = monadicIO $ do
-  -- run (putStrLn "new")
-
-  let seed' = succ (fromIntegral (seed `mod` 40))
+  let seed' = 8 + (fromIntegral (seed `mod` 40))
   key ← run (keygen seed')
 
-  run (printf "key: %d\n" (getShared key) :: IO ())
+  -- run (printf "key: %d\n" (getShared key) :: IO ())
   cipher ← run (encrypt seed' key bit)
 
   let bit' = decrypt key cipher
-  -- run (putChar '\n')
   assert (bit == bit')
 
-foo ∷ Int → Bit → IO Bit
-foo η msg = do
-  key ← keygen (fromIntegral η)
+-- homomorphic 
+-- c₁    = q₁p + 2r₁ + m₁
+-- c₂    = q₂p + 2r₂ + m₂
+-- c₁+c₂ = (q₁+q₂)p + 2(r₁+r₂) + m₁ + m₂
 
-  c     ← encrypt (fromIntegral η) key msg
-  let d = decrypt key c
+prop_plus ∷ Word8 → Bit → Property
+prop_plus seed bit = monadicIO $ do
+  let η = 8 + (fromIntegral (seed `mod` 40))
+
+  times ∷ Integer ← pick $ choose (1, 10)
   
-  return d
+  msg ∷ [Bit] ← pick $ vector (fromIntegral times)
+  
+  key ← run (keygen η)
+
+  ciphers ∷ [Integer] ← run $ mapM (encrypt η key) msg
+  -- ciphers ∷ [[Integer]] ← run $ replicateM (fromIntegral times) (mapM (encrypt η key) msg)
+  
+  -- bits ← decrypt key (mult4 cipher)
+
+  -- assert (mult4 (toInt bit) == toInt bit')
+  undefined
+
+-- enc ∷ Integer → Bit → IO Integer
+-- enc η msg = do
+--   -- let η = 8 + (fromIntegral (seed `mod` 40))
+  
+--   key ← keygen η
+
+--   encrypt η key msg
+
 
