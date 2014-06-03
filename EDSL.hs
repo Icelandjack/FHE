597c2a15cf9103f9d412ba3d1f88fdb0a51355ff
Baldur Blöndal <baldurpet@gmail.com>
2014-06-03 23:39:53 +0200

    aes

--- EDSL.hs
+++ EDSL.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RebindableSyntax, GADTs, TypeFamilies, UnicodeSyntax, UndecidableInstances, ScopedTypeVariables #-}
+{-# LANGUAGE RebindableSyntax, GADTs, TypeFamilies, UnicodeSyntax, UndecidableInstances, ScopedTypeVariables, DeriveDataTypeable, PatternSynonyms #-}
 
 module EDSL where
 
@@ -6,11 +6,18 @@ import qualified Prelude as P
 import Prelude hiding ((<), (>=), (<=), min, max)
 import Data.Bits
 import Data.Word
+import Data.List
 import Data.Maybe
+import Data.Generics.Text
+import Data.Data
+import GHC.TypeLits
 
 infixr 2 :||
 infix  4 .==
 infix  4 :==
+
+pattern a :<< as ← ((\xs → case null xs of { True → Nothing; _ → Just (head xs, tail xs)}) → Just (a, as))
+pattern as :>> a ← ((\xs → case null xs of { True → Nothing; _ → Just (init xs, last xs)}) → Just (as, a))
 
 eval ∷ Exp a → a
 eval (Lit i) = i
@@ -18,8 +25,8 @@ eval (LitB b) = b
 eval (If c t e) = case eval c of
   True  → eval t
   False → eval e
-eval (While c b i) =
-  head $ dropWhile (eval . c . Value) $ iterate (eval . b . Value) (eval i)
+eval (While c b i) = error "while"
+  -- head $ dropWhile (eval . c . Value) $ iterate (eval . b . Value) (eval i)
 eval (ArrLen arr) = length (eval arr)
 eval (Arr l ixf) = let
   len = eval l
@@ -37,8 +44,11 @@ eval (Pair a b) = (eval a, eval b)
 eval (Fst a) = fst (eval a)
 eval (Snd a) = snd (eval a)
 eval (Value a) = a
-eval (Lookup l ixf) = map ixf (take (eval l) [0..])
-eval _   = undefined
+-- eval (Lookup l ixf) = map ixf (take (eval l) [0..])
+eval (Table a)   = a
+eval (TableIndex table i) = eval table !! fromIntegral (eval i)
+eval (Arr2 p ixf) = Vec (map (eval . ixf) [0..fromIntegral (natVal p - 1)])
+eval (Prim2 _ f a b) = f (eval a) (eval b)
 
 min ∷ Exp Int → Exp Int → Exp Int
 min a b = ifC (a < b) a b
@@ -49,15 +59,17 @@ max a b = ifC (a < b) b a
 --   Let :: String -> Lam expr (ra -> (ra -> rb) -> rb) (a -> (a -> b) -> b)
 --   Inject :: expr role a -> Lam expr role a
 
+newtype LenVec (n ∷ Nat) a = Vec [a] deriving Show
+
 data Exp a where
   -- | Lambda expressions
   Var    ∷ String → Exp a
-  Value  ∷ a → Exp a
+  Value  ∷ Show a ⇒ a → Exp a
   Lambda ∷ (Exp a → Exp b) → Exp a → Exp b
   (:·)   ∷ Exp (a → b) → Exp a → Exp b
   Let    ∷ String → Exp (a → (a → b) → b)
 
-  Lit    ∷ Num a ⇒ a → Exp a
+  Lit    ∷ (Show a, Num a) ⇒ a → Exp a
   LitB   ∷ Bool → Exp Bool
   If     ∷ Exp Bool → Exp a → Exp a → Exp a
   While  ∷ (Exp s → Exp Bool) → (Exp s → Exp s) → Exp s → Exp s
@@ -79,27 +91,56 @@ data Exp a where
   
   (:||)  ∷ Exp Bool → Exp Bool → Exp Bool
 
+  Arr2   ∷ KnownNat n ⇒ Proxy n → (Int → Exp a) → Exp (LenVec n a)
+
   -- Arrays
   Arr    ∷ Exp Int → (Exp Int → Exp a) → Exp [a]
   ArrLen ∷ Exp [a] → Exp Int
   ArrIx  ∷ Exp [a] → Exp Int → Exp a
 
   -- Lookup table?
-  Lookup ∷ Exp Int → (Word8 → Word8) → Exp [Word8]
+  Table      ∷ [Word8] → Exp [Word8]
+  TableIndex ∷ Exp [Word8] → Exp Word8 → Exp Word8
 
-  Undef  ∷ Exp a
+  Undef  :: Exp a
 
   GetBit ∷ Exp Word8 → Exp Int → Exp Word8
 --   SetBit ∷ Exp Word8 → Exp Int → Exp 
 
-instance Show a ⇒ Show (Exp a) where
-  show (Lit i) = show i
-  show (LitB b) = show b
-  show (Var x)  = x
-  show (If c t e) = "(if " ++ show c ++ " then " ++ show t ++ " else " ++ show e ++ ")"
-  show (a :== b) = "(" ++ show a ++ " == " ++ show b ++ ")"
-  show Undef = "UNDEF"
-  show a = show (eval a)
+instance Show (Exp a) where
+  show = \case
+    Arr l ixf    → "INDEXED " ++ show l ++ " something"
+    Lit i        → show i
+    LitB b       → show b
+    Var x        → x
+    If c t e     → "(if " ++ show c ++ " then " ++ show t ++ " else " ++ show e ++ ")"
+    a :== b      → "(" ++ show a ++ " == " ++ show b ++ ")"
+    Undef        → "UNDEF"
+    a :+ b       → "(" ++ show a ++ " + " ++ show b ++ ")"
+    Arr2 l ixf   → let
+      len ∷ Int
+      len = fromInteger (natVal l)
+      
+      removeParen ∷ String → String
+      removeParen ('(' :<< xs :>> ')') = removeParen xs
+      removeParen xs                   = xs
+
+      values ∷ [String]
+      values = map (removeParen . show . ixf) [0..len - 1]
+      in "(ArrStaticLen<" ++ show len ++ "> [" ++ intercalate ", " values ++ "]>)"
+
+    -- | Addition in GF(2⁸)
+    Prim2 "GAdd" _ a b → "(" ++ show a ++ " ⊕ " ++ show b ++ ")"
+    
+    -- | Multiplication in GF(2⁸)
+    Prim2 "GMul" _ a b → "(" ++ show a ++ " • " ++ show b ++ ")"
+
+    -- | Table operations
+    Table word       → take 14 (show word) ++ "…]"
+
+    TableIndex table index → show table ++ "‼" ++ show index
+    
+    a                → "."
 
 class Embed a where
   type Repr a
@@ -166,7 +207,7 @@ a .== b = a :== b
 mod' ∷ Exp Int → Exp Int → Exp Int
 mod' a b = while (>= b) (subtract a) a
 
-instance Num a ⇒ Num (Exp a) where
+instance (Show a, Num a) ⇒ Num (Exp a) where
   fromInteger = Lit . fromInteger
   
   Lit a + Lit b = Lit (a + b)
@@ -215,10 +256,10 @@ instance Embed a ⇒ Embed (Vector a) where
   desugar (Indexed l ixf) = Arr l (desugar . ixf)
   sugar arr           = Indexed (len arr) (arr <!>)
 
-fromList ∷ [Word8] → Vector (Exp Word8)
-fromList xs = indexed (Lit (length xs)) indexFunction where
+-- fromList ∷ [Word8] → Vector (Exp Word8)
+-- fromList xs = indexed (Lit (length xs)) indexFunction where
 
-  indexFunction (Lit i) = Lit (fromJust (lookup i (zip [0..] xs)))
+--   indexFunction (Lit i) = Lit (fromJust (lookup i (zip [0..] xs)))
 
 instance (Embed a, Repr a ~ b, Show b) ⇒ Show (Vector a) where
   show a = show (desugar a)
@@ -261,3 +302,17 @@ instance Functor Vector where
 scalarProd ∷ (Embed a, Num a) ⇒ Vector a → Vector a → a
 scalarProd a b = sumVec (zipWithVec (*) a b)
 
+fromList ∷ ∀n a. KnownNat n ⇒ [Exp a] → Maybe (Exp (LenVec n a))
+fromList xs
+  | fromIntegral (length xs) == len = Just (Arr2 proxyLen (atIndex xs))
+  | otherwise                       = Nothing
+
+  where
+    proxyLen ∷ Proxy n
+    proxyLen = Proxy
+    
+    len ∷ Integer
+    len = natVal proxyLen
+    
+    atIndex ∷ [b] → Int → b
+    atIndex xs n = fromJust (lookup n (zip [0..] xs))
--- aes.hs
+++ aes.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE RebindableSyntax, GADTs, TypeFamilies, UnicodeSyntax, PatternSynonyms, LambdaCase, ViewPatterns #-}
+{-# LANGUAGE RebindableSyntax, GADTs, TypeFamilies, UnicodeSyntax, PatternSynonyms, LambdaCase, ViewPatterns, InstanceSigs, LiberalTypeSynonyms #-}
 
 module AES where
 
 import EDSL
+import Pretty
 
 import Prelude
 import Data.List
+import qualified Data.Text as T
 import Data.Word
 import Data.Maybe
 import Data.Bits
@@ -26,63 +28,50 @@ bits n = showIntAtBase 2 intToDigit n ""
 
 data Matrix a = M [[a]] deriving (Show, Eq)
 
+instance Functor Matrix where
+  fmap f (M ass) = M (fmap (fmap f) ass)
+
 instance Embed (Matrix (Exp Word8)) where
   type Repr (Matrix (Exp Word8)) = [Word8]
-  sugar (Arr 16 ixf) = M (chunksOf 4 (map (ixf . Lit) [0..15]))
-  sugar Arr{}        = error "length mismatch"
 
-  desugar (M mss) = Arr 16 (foo (concat mss)) 
+  sugar   = sugarMatrix
+  desugar = desugarMatrix
 
-foo mss (Lit n) = mss !! n
-foo mss a       = if a .== 0
-  then mss !! 0
-  else if a .== 1
-  then mss !! 1
-  else if a .== 2
-  then mss !! 2
-  else if a .== 3
-  then mss !! 3
-  else if a .== 4
-  then mss !! 4
-  else if a .== 5
-  then mss !! 5
-  else if a .== 6
-  then mss !! 6
-  else if a .== 7
-  then mss !! 7
-  else if a .== 8
-  then mss !! 8
-  else if a .== 9
-  then mss !! 9
-  else if a .== 10
-  then mss !! 10
-  else if a .== 11
-  then mss !! 11
-  else if a .== 12
-  then mss !! 12
-  else if a .== 13
-  then mss !! 13
-  else if a .== 14
-  then mss !! 14
-  else if a .== 15
-  then mss !! 15
-  else Undef
+
+sugarMatrix ∷ Exp [Word8] → Matrix (Exp Word8)
+sugarMatrix (Arr 16 ixf) = M (chunksOf 4 (map (ixf . Lit) [0..15]))
+sugarMatrix Arr{}        = error "length mismatch"
+
+desugarMatrix ∷ Matrix (Exp Word8) → Exp [Word8]
+desugarMatrix (M mss)
+    | length (concat mss) == 16 = Table (concat mss)
+    | otherwise                 = error "desugar: length mismatch"
 
 -- AES
 -- SubBytes()
 -- ShiftRows()
 -- MixColumns()
 -- AddRoundKey()
-subBytes ∷ [Word8] → Matrix Word8 → Matrix Word8
-subBytes (fromList → sbox) (M mms) = M mms where
-  
-  sBoxLookup ∷ Exp Word8 → Exp Word8
-  sBoxLookup key = sbox ! Conv key
 
-makeLookup ∷ [Word8] → Exp [Word8]
-makeLookup box = Lookup (Lit (length box)) undefined where
-  foo ∷ Word8 → Word8
-  foo i = fromJust (lookup i (zip [0..] sBox))
+plusOne ∷ Matrix (Exp Word8) → Matrix (Exp Word8)
+plusOne = fmap (+1)
+
+subBytes' ∷ [Word8] → Matrix Word8 → Matrix Word8
+subBytes' (fromList → sbox) = undefined
+
+-- subBytes ∷ Exp [Word8] → a
+subBytes = Lambda undefined
+
+-- subBytes ∷ [Word8] → Matrix Word8 → Matrix Word8
+-- subBytes (fromList → sbox) (M mms) = M mms where
+  
+--   sBoxLookup ∷ Exp Word8 → Exp Word8
+--   sBoxLookup key = sbox ! Conv key
+
+-- makeLookup ∷ [Word8] → Exp [Word8]
+-- makeLookup box = Lookup (Lit (length box)) undefined where
+--   foo ∷ Word8 → Word8
+--   foo i = fromJust (lookup i (zip [0..] sBox))
 
 sBox ∷ [Word8]
 sBox = [0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76, 0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0, 0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15, 0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75, 0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84, 0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF, 0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8, 0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2, 0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73, 0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB, 0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79, 0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08, 0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A, 0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E, 0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF, 0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16]
@@ -99,7 +88,8 @@ cycle' [] _                       = []
 cycle' xs 0                       = xs
 cycle' xs ((`mod` length xs) → n) = drop n xs ++ take n xs
 
-prop_cycle n xs = cycle' xs (length xs) == xs
+prop_cycle ∷ Eq a ⇒ [a] → Bool
+prop_cycle xs = cycle' xs (length xs) == xs
 
 example ∷ Matrix (Exp Word8)
 example = M [ [0, 1, 2, 3]
@@ -109,7 +99,11 @@ example = M [ [0, 1, 2, 3]
             ]
 
 -- | Shifting by powers of 24 is idempotent
-prop_shiftRows matrix (abs → n) = iterate shiftRows matrix !! (2*3*4*n) == matrix
+prop_shiftRows ∷ Eq a ⇒ Matrix a → Int → Bool
+prop_shiftRows matrix (abs → n) =
+  iterate shiftRows matrix !! (2*3*4*n)
+  ==
+  matrix
 
 mixColumns (M (transpose → mms)) = M undefined where
   foo = undefined
@@ -139,13 +133,12 @@ mixColumns (M (transpose → mms)) = M undefined where
 --   -- First row (0, 4, 8, 12) is unchanged
 --   modify ixf n = ixf n
 
--- mixColumns = undefined
--- addRoundKey = undefined
+addRoundKey = undefined
 
-aes ∷ Matrix Word8 → Matrix Word8 → Matrix Word8
-aes inBlock key = for 10 inBlock step where
+-- aes ∷ Matrix Word8 → Matrix Word8 → Matrix Word8
+-- aes inBlock key = for 10 inBlock step where
 
-  step _ = addRoundKey . mixColumns . shiftRows . subBytes sBox 
+--   step _ = shiftRows . subBytes sBox 
 
 -- addRoundKey ∷ Matrix Word8 → Matrix Word8 
 -- addRoundKey = undefined
@@ -162,4 +155,6 @@ aes inBlock key = for 10 inBlock step where
 
 instance Arbitrary a ⇒ Arbitrary (Matrix a) where
   arbitrary = M <$> vectorOf 4 (vector 4) 
+
+data Querying k v a = Querying [k] ([(k, v)] → a)
 
